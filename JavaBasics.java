a8198dd0307b98710ed973bca7bf6d3d484bdbf0
Nikunj Kumar <nikunjkumar6379@gmail.com.com>
2024-09-19 22:38:34 +0530

    Boilerplate code for Java

---
+++ JavaBasics.java
@@ -0,0 +1,11 @@
+// Boilerplate code: The code which is a default code for any programing language is called "BoilerPlate Code".
+//This code will be written everytime when we start any java programming.
+
+public class JavaBasics {
+    public static void main (String arg[]){
+
+
+        
+    }
+
+}
