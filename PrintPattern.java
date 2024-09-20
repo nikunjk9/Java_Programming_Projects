ca6ed801a9e1f699318d5a7fb2d52b12ffbea487
Nikunj Kumar <nikunjkumar6379@gmail.com.com>
2024-09-20 22:46:56 +0530

    Printing Patterns

---
+++ PrintPattern.java
@@ -0,0 +1,9 @@
+public class PrintPattern {
+    public static void main(String arg[]){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+    }
+}
