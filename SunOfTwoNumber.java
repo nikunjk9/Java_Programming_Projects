94e0baa8852fe2cf4206cb88e63d953dadbced47
Nikunj Kumar <nikunjkumar6379@gmail.com.com>
2024-09-23 12:40:26 +0530

    Sumof two numbers

---
+++ SunOfTwoNumber.java
@@ -0,0 +1,25 @@
+// Sum of two number
+
+public class SunOfTwoNumber{
+    public static void main(String arg[]){
+
+        int a = 100;
+        int b = 200;
+        int sum = a + b;
+        System.out.println(sum);
+
+        //Let's give new values to numbers
+        a = 12;  //To change value of a variable in the middle of the code 
+        b = a;
+        int Sum = a + b;
+        System.out.print(Sum);
+
+    }
+}
+
+/*
+ * This is also a way to write comments in the code 
+ * using this we don't have to
+ * write // in every line
+ * we can write comment in till we want using this 
+ */
