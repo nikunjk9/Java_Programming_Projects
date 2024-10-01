a867e4e9ba85ff1c79a4fa521442a5cbf6b1f996
Aryan Nandanwar <116783328+AryanNandanwar@users.noreply.github.com>
2024-10-02 00:51:39 +0530

    Update ParameterOfRectangle.java
    Reduced redundancy in calculating perimeter of rectangle

--- ParameterOfRectangle.java
+++ ParameterOfRectangle.java
@@ -3,16 +3,13 @@
 
 import java.util.*;
 
-public class ParameterOfRectangle{
+public class PerimeterOfRectangle{
     public static void main(String arg[]){
         Scanner sc = new Scanner(System.in);
         float l = sc.nextFloat();
         float b = sc.nextFloat();
-        float sum = l + b;
-        System.out.println(sum);
-
-        float parameter = 2*sum;
-        System.out.print(parameter);
+        float perimeter = 2*(l + b);
+        System.out.print(perimeter);
         sc.close();
     }
 }
