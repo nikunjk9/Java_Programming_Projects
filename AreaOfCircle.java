5d767c2894faed5c028eb93cc4bfaea72ba0f44a
Nikunj Kumar <nikunjkumar6379@gmail.com>
2024-10-05 01:49:59 +0530

    Update AreaOfCircle.java

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -1,12 +1,14 @@
 // Area of circle = 3.14*r*r
 
 import java.util.*;
+// import java.lang.Math;          we can also use math fuction to print area of circle
 
 public class AreaOfCircle{
     public static void main(String arg[]){
         Scanner sc = new Scanner(System.in);
         float r = sc.nextFloat();                   // Radius should be float cause it can be in decimal 
         float area = 3.14f*r*r;                  // As 3.14 is a float value so we have to take datatype as float.
+        // float area = 3.14f*Math.pow(r,2);             Using math function 
         System.out.print(area);
         sc.close();
 
