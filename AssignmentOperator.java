78442157699dcfe3d9c06c9b8c4586af10413662
nikunjk9 <nikunjkumar6379@gmail.com>
2024-10-07 22:13:13 +0530

    Assignment Operator

---
+++ AssignmentOperator.java
@@ -0,0 +1,53 @@
+// Assignment Operator
+
+
+public class AssignmentOperator {
+    public static void main(String arg[]){
+        
+        // Addition: +=
+        int a = 10;
+        System.out.println("Original value of a :" + a);
+        a += 10;                                               // Another way of writing (a = a + 10;)
+        System.out.println("Updated value of a :" + a);
+
+
+        // Substraction: -=
+        int b = 5;
+        System.out.println("Original value of b :" + b);
+        b -= 2;                                                 // Another way of writing (b = b - 2;)
+        System.out.println("Updated value of a :" + b);
+
+
+        // Multiplication: *=
+        int c = 20;
+        System.out.println("Original value of c :" + c);
+        c *= 2;                                                  // Another way of writing (c = c * 2;)
+        System.out.println("Updated value of c :" + c);
+
+
+        // Division: /=
+        int d = 40;
+        System.out.println("Original value of d :" + d);
+        d /= 2;                                                  // Another way of writing (d = d/2;)
+        System.out.println("Updated value of d :" + d);
+
+
+        // Modulo: %=
+        int e = 10;
+        System.out.println("Original value of d :" + e);
+        e %= 2;                                                  // Another way of writing (e = e%2;)
+        System.out.println("Updated value of d :" + e);
+
+
+
+
+
+
+
+
+
+    
+
+    }
+    
+}
