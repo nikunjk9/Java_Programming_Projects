28d36abd245b9999b80e89922cc8bef7c5ab811c
nikunjk9 <nikunjkumar6379@gmail.com>
2024-10-18 20:49:51 +0530

    Ternary Operator

---
+++ Condition Statement/CheckMarks.java
@@ -0,0 +1,22 @@
+
+// Check if a student will pass or fail
+
+import java.util.*;
+
+public class CheckMarks {
+    public static void main(String arg[]){
+
+        Scanner sc = new Scanner(System.in){
+            int marks = sc.nextInt();                                  // Taking user marks as input
+
+            String result = (marks >= 33) ? "Pass" : "Fail";           // If marks is greater than equal to 33 then print Pass if not print fail
+            System.out.println("Your result is:" + " " + result);     
+
+            sc.close();
+
+        }
+
+    
+    }
+    
+}
--- Condition Statement/TernaryOperator.java
+++ Condition Statement/TernaryOperator.java
@@ -9,8 +9,11 @@ public class TernaryOperator {
         int number = sc.nextInt();                                      // Taking input from user
 
         //Ternary Operator to check if number is Odd or Even
-        String type = ((number)%2 == 0) ? "Even" : "Odd";               // 
+        String type = ((number)%2 == 0) ? "Even" : "Odd";               // if rimender of number divided by 2 is 0 then print Even if not Print Odd
         System.out.println("The type of number is:" + " " + type);
+
+
+        sc.close();
     }
     
 }
